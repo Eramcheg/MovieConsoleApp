911e94ce82d7314c4a0919f470957bb68e390a31
Illia Salohub <eramcheg@gmail.com>
2024-04-21 17:50:44 +0200

    Implemented basic 3 functions of adding, loading and getting movies list

--- src/main/java/com/illiasalohub/movieapp/model/MovieList.java
+++ src/main/java/com/illiasalohub/movieapp/model/MovieList.java
@@ -1,4 +1,25 @@
 package com.illiasalohub.movieapp.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MovieList {
+    private List<Movie> movies = new ArrayList<>();
+    private DataProcessor dataManager = new DataProcessor();
+
+    public MovieList() {
+        this.movies = dataManager.loadMovies();  // Load movies at construction
+    }
+
+    public void addMovie(Movie movie) {
+        movies.add(movie);
+    }
+
+    public void saveMovies() {
+        dataManager.saveMovies(movies);
+    }
+
+    public List<Movie> getMovies() {
+        return movies;
+    }
 }
