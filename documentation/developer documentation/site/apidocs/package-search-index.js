5ba18774439fecbcd6b71a9b8f4ec80cec67b8df
Illia Salohub <eramcheg@gmail.com>
2024-05-07 23:20:07 +0200

    Architecture fix, added documentation folder

---
+++ documentation/developer documentation/site/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.illiasalohub.movieapp"},{"l":"com.illiasalohub.movieapp.controller"},{"l":"com.illiasalohub.movieapp.model"},{"l":"com.illiasalohub.movieapp.services"},{"l":"com.illiasalohub.movieapp.views"}];updateSearchResults();
